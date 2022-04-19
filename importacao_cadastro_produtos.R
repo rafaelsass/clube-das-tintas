8260abd7a331ba035fba6e568b65b3b74406b8c0
rafaelsass <rafaelseass@gmail.com>
2022-04-19 19:17:03 -0300

    correção

--- importacao_cadastro_produtos.R
+++ importacao_cadastro_produtos.R
@@ -2,70 +2,62 @@ library(readxl)
 library(dplyr)
 library(stringr)
 
-
 # importacao de dados -----------------------------------------------------
 
 
 fornecedor <- read_excel("dados/importacao/blascor/blascor_0322.xlsx", skip = 1)
 interno <- read_excel("dados/sede_0222.xlsx", skip = 1)
 
+interno <- interno %>%
+  filter(`Código de fábrica` %in% fornecedor$codigo[is.na(fornecedor$codigo) == F] & 
+           Fornecedor %in% c()) %>% unique() %>%
+  arrange(as.numeric(`Código de fábrica`)) 
 
- #Uso com codigo de fábrica --------------------------------------------
-
-
- interno <- interno %>%
-   filter(`Código de fábrica` %in% fornecedor$codigo[is.na(fornecedor$codigo) == F] & 
-            Fornecedor %in% c(90001943,90000011,90000919)) %>% unique() %>%
-   arrange(as.numeric(`Código de fábrica`)) 
-
- fornecedor <- fornecedor %>%
-   filter(codigo %in% interno$`Código de fábrica`) %>%
-   arrange(as.numeric(codigo))
+fornecedor <- fornecedor %>%
+  filter(codigo %in% interno$`Código de fábrica`) %>%
+  arrange(as.numeric(codigo))
 
 # criacao de tabela para importacao ---------------------------------------
-
 # necessaria a adaptaçao dos valores e colunas para importacao no sistema
-organizar_colunas <- function(data1, data2){
-  table <- select(data2, Cód.Item) %>%
-      mutate(#`Código de Barras` = data1$cod_barras,
-      Cód.Item = str_pad(data2$Cód.Item,5,"left",0))
-  
-  df_pad <- data.frame(matrix(
-    vector(), 0, 51,dimnames = (list(c(),c( #0-50 com código de barras 0-51 sem
-      LETTERS[2:26],paste0("A",LETTERS))))), #2;26 com cod barras 3;26 sem
-    stringsAsFactors=F)
-  
-  table <- full_join(table, df_pad,by = character()) %>%
-    mutate(#S = data1$NCM,
-      #Z = str_replace_all(data1$Peso_Bruto,"\\.",","),
-      #AE = data1$Mult_venda,
-      AI = str_replace_all(data1$`Preco_Compra`,"\\.",","),
-      AJ = data1$desconto,
-      E = str_pad(data2$Departamento, 3, "left", pad = 0),
-      #G = data2$Descrição
-      )
-}
-table <- organizar_colunas(fornecedor,interno)
 
-# exporta??o da tabela finalizada -----------------------------------------
+tabela <- data.frame(matrix(
+  vector(), nrow(fornecedor), 52,dimnames = (list(c(),c(
+    LETTERS,paste0("A",LETTERS))))), 
+  stringsAsFactors=F)
 
+tabela <- tabela %>%
+  mutate(A = str_pad(interno$Cód.Item,5,"left",0),
+         B = interno$cod_barras,
+         #S = fornecedor$NCM,
+         #Z = str_replace_all(fornecedor$Peso_Bruto,"\\.",","),
+         #AE = fornecedor$Mult_venda,
+         AI = str_replace_all(fornecedor$`Preco_Compra`,"\\.",","),
+         AJ = fornecedor$desconto,
+         E = str_pad(interno$Departamento, 3, "left", pad = 0),
+         #G = interno$Descrição
+  )
 
+# exportação da tabela finalizada -----------------------------------------
+
+#import A e AA
+write.table(filter(table, E == "001"| E == "002"), 
+            file = "arq/data_import_sw_222_AAA.csv", quote = F, sep = ";", 
+            row.names = F, col.names = F, dec = ",", na = "")
+#import B e C
 write.table(filter(table, E == "003"| E == "004"), 
             file = "arq/data_import_sw_222_BC.csv", quote = F, sep = ";", 
             row.names = F, col.names = F, dec = ",", na = "")
 
-
-
 # criação de tabela para checagem de diferença em produtos ----------------
 
-check <- interno %>%
+checagem <- interno %>%
   mutate(Preço_Sistema = Pr.Compra, 
          Preço_Tabela = as.numeric(fornecedor$Preco_Compra),
          Variação = (Preço_Tabela/Preço_Sistema) - 1) %>%
   select(Cód.Item, Descrição, `Código de fábrica`, Preço_Sistema, Preço_Tabela, 
          Variação, Departamento) %>%
   filter(Variação != 0)
-  
+
 
 write.table(check, file = "arq/data_check_sw.csv", quote = F, sep = ";", 
             row.names = F, col.names = T, dec = ",", na = "")
